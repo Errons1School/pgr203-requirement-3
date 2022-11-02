ee0894ba494841ce64a17e14c2e2d0349354786f
Snorre <snle001@student.kristiania.no>
2022-11-02 13:27:03 +0100

    Endpoint uses JSON and DAO for only.

--- src/main/java/no/kristiania/webshop/ProductEndpoint.java
+++ src/main/java/no/kristiania/webshop/ProductEndpoint.java
@@ -1,61 +1,30 @@
 package no.kristiania.webshop;
 
 import jakarta.inject.Inject;
-import jakarta.json.Json;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import no.kristiania.webshop.db.ProductDao;
 
-import java.io.StringReader;
-import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.util.List;
 
 
 @Path("/products")
 public class ProductEndpoint {
 
     @Inject
-    private ProductDao dao;
+    private ProductDao productDao;
 
     @GET
-    public Response getAllProducts() throws URISyntaxException, SQLException {
-        var result = Json.createArrayBuilder();
-        var products = dao.getAllProduct();
-        for (var prod : products) {
-            result.add(Json.createObjectBuilder()
-                    .add("name", prod.getName())
-                    .add("category", prod.getCategory())
-                    .add("img", prod.getImg())
-                    .add("description", prod.getDescription())
-                    .add("price", prod.getPrice())
-                    .add("stock", prod.getStock())
-            );
-        }
-
-        System.out.println("Successful get request!");
-
-        return Response.ok(result.build().toString()).build();
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Product> getAllProducts() throws SQLException {
+        return productDao.getAllProduct();
     }
 
     @POST
-    public Response addProducts(String body) throws SQLException {
-        var reader = new StringReader(body);
-        var jsonBody = Json.createReader(reader).readObject();
-
-        var tmpProd = new Product(
-                jsonBody.getString("name"),
-                jsonBody.getString("category"),
-                jsonBody.getString("img"),
-                jsonBody.getString("description"),
-                jsonBody.getInt("price"),
-                jsonBody.getInt("stock")
-        );
-
-        dao.saveProduct(tmpProd);
-        System.out.println("Added Product! Name:'" + tmpProd.getName() + "'");
-        return Response.ok().build();
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addProducts(Product product) throws SQLException {
+        productDao.saveProduct(product);
     }
 
 }
